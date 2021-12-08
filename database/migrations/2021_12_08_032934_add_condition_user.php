4a77ae29488f6a8be91bc284ab8b6951aed98f24
Jennifer <jeante05@gmail.com>
2021-12-07 22:41:50 -0500

    feat: migration rol and add_columns user

---
+++ database/migrations/2021_12_08_032934_add_condition_user.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddConditionUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone_number');
+            $table->string('address');
+            $table->foreignId('rol_id')->nullable()->constrained('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('phone_number');
+            $table->dropColumn('address');
+            $table->dropForeign(['rol_id']);
+        });
+    }
+}
