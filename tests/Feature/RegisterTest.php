a154ee97a9391a576ec75d01c447ecd4a43d8b05
Jennifer <jeante05@gmail.com>
2021-12-10 11:43:47 -0500

    feat:creation test

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class RegisterTest extends TestCase
@@ -16,11 +18,12 @@ class RegisterTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_new_users_can_register(): void
+    /**
+     * @dataProvider userProvider
+     */
+    public function test_new_users_can_register(string $name, string  $email,string  $password, string  $password_confirmation, string $phone_number, string  $address ): void
     {
-        $this->withoutExceptionHandling();
-        $data = $this->registerUserData();
-        $response = $this->post('/register', $data);
+        $response = $this->post('/register', compact('name','email','password', 'password_confirmation', 'phone_number', 'address'));
         
         $this->assertDatabaseHas('users',[
             'name' => 'Jennifer',
@@ -30,15 +33,55 @@ class RegisterTest extends TestCase
         ]);  
     }
 
-    public function registerUserData(): array
+    /**
+     * @dataProvider invalidDataProvider
+     */
+    public function test_it_validate_request_data_register(string $name,string  $email,string  $password, string  $password_confirmation, string $phone_number, string  $address, string $field): void
+    {
+        $response = $this->post('/register', compact('name','email','password', 'password_confirmation', 'phone_number', 'address'));
+        //$response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        
+        $response->assertInvalid([$field]);
+    }
+
+     /**
+     * @dataProvider userProvider
+     */
+    public function test_email_is_unique(string $name,string  $email,string  $password, string  $password_confirmation, string $phone_number, string  $address): void
+    {
+        $user= User::factory()->create(compact('name','email','password', 'phone_number', 'address'));
+        $this->test_it_validate_request_data_register($name, $email, $password, $password_confirmation, $phone_number, $address, 'email');
+    }
+
+
+    public function invalidDataProvider(): array
+    {
+        $data = $this->userProvider()['user'];
+        
+        return [
+            'name required' => array_merge($data, ['name' => '', 'field' => 'name']),
+            'name max' => array_merge($data, ['name' => Str::random(256), 'field' => 'name']),
+            'email required' => array_merge($data, ['email' => '', 'field' => 'email']),            
+            'email max' => array_merge($data, ['email' => Str::random(255), 'field' => 'email']),            
+            'password required' => array_merge($data, ['password' => '', 'field' => 'password']),            
+            'password min' => array_merge($data, ['password' => 'jen', 'field' => 'password']), 
+            'phone_number max' => array_merge($data, ['phone_number' => Str::random(256), 'field' => 'phone_number']),
+            'address max' => array_merge($data, ['address' => Str::random(256), 'field' => 'address']),           
+            
+        ];
+    }
+
+    public function userProvider(): array
     {
         return [
+            "user" => [
             'name' => 'Jennifer',
             'email' => 'jeante18@gmail.com',
             'password' =>'jeante18',
             'password_confirmation' => 'jeante18',
             'phone_number' => '31243435',
-            'address' => 'carrera 1 #1-1',
+            'address' => 'carrera 1 #1-12',
+            ]
         ];
     }
 }
